ac322edad05bd7751f45abf3a8b4a0b645d13f0c
Ge Rui Sen <greyson.rg@gmail.com>
2025-06-23 15:39:21 +0800

    Removed unused files and data

--- webScraping/test.py
+++
@@ -1,8 +0,0 @@
-import urllib.parse
-
-base_url = "https://www.anandtech.com/tag/mb"
-current_link_idx = 2
-
-page_link = urllib.parse.urljoin(base_url, str(current_link_idx))
-page_link = base_url + r"/" + str(current_link_idx)
-print(f"Next page link: {page_link}")
